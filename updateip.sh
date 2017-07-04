e0782407eb2dd32cd3aac337b90debe78d8fc454
Kevin Shieh <kshieh.kevin@gmail.com>
2017-07-04 16:18:29 +0800

    Automatic save commit initiated at Tue Jul  4 16:18:29 CST 2017

--- updateip.sh
+++ updateip.sh
@@ -5,14 +5,12 @@ IPADDRESS=$(curl -s http://ipv4.icanhazip.com/)
 echo "Last check at: $(date)" >> updateip.log
 
 if [[ "${IPADDRESS}" != $(cat ~/updateip/current_ip) ]];then
-#if echo "${IPADDRESS}" | mutt -s "new ip" myemail@com ;then
-#echo "Ip change from $(cat ~/updateip/current_ip) to ${IPADDRESS}" >> updateip.log
+
+echo "Ip change from $(cat ~/updateip/current_ip) to ${IPADDRESS}" >> updateip.log
 echo ${IPADDRESS} > ~/updateip/current_ip
 
 git add -u
 git commit -m "Automatic save commit initiated at $(date)"
-#git push
-#else
-#echo "Failed to send the mail, try again later." >> updateip.log
-#fi
+git push
+
 fi
