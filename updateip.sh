eda68a924e8df6f7f669dd2c92ac668e172d3737
Kevin Shieh <kshieh.kevin@gmail.com>
2017-07-04 17:10:02 +0800

    Automatic save commit initiated at Tue  4 Jul 17:10:02 CST 2017

--- updateip.sh
+++ updateip.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# required for using this script in crontab
+cd /home/pi/updateip
+
 IPADDRESS=$(curl -s http://ipv4.icanhazip.com/)
 
 echo "Last check at: $(date)" >> updateip.log
 
-if [[ "${IPADDRESS}" != $(cat ~/updateip/current_ip) ]];then
+if [[ "${IPADDRESS}" != $(cat current_ip) ]];then
 
-echo "Ip change from $(cat ~/updateip/current_ip) to ${IPADDRESS}" >> updateip.log
-echo ${IPADDRESS} > ~/updateip/current_ip
+echo "Ip change from $(cat current_ip) to ${IPADDRESS}" >> updateip.log
+echo ${IPADDRESS} > current_ip
 
 git add -u
 git commit -m "Automatic save commit initiated at $(date)"
